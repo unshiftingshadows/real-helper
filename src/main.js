956a36c6220267b2115f54bd2281e77e96cee422
Jacob Beck <jacob@unshiftingshadows.com>
2018-09-14 17:37:32 -0500

    One last test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import wrap from '@vue/web-component-wrapper'
 
-const ContentEditor = wrap(Vue, () => import('./components/ContentEditor.vue'))
+// const ContentEditor = wrap(Vue, () => import('./components/ContentEditor.vue'))
+const ContentModule = wrap(Vue, () => import('./components/modules/Content.vue'))
 
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(ContentEditor)
+  // window.Vue.use(ContentEditor)
+  window.Vue.use(ContentModule)
 }
 
-export default ContentEditor
+export default ContentModule
